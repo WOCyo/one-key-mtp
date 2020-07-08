127e9810772b74f9f02471781f043166a4ee6b75
Pipixia <56287117+pipixia244@users.noreply.github.com>
2020-07-09 02:33:16 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -78,4 +78,4 @@ echo -e "\033[32mtg://proxy?server=$ip&port=$num&secret=ee$secret$hexdomain\033[
 echo -e "\033[36mtg://proxy?server=$ip&port=$num&secret=dd$secret\033[0m" > /etc/mtproxy/secret
 echo -e "\033[36mtg://proxy?server=$ip&port=$num&secret=ee$secret$hexdomain\033[0m" >> /etc/mtproxy/secret
 echo -e "\033[36m后续查看配置信息可使用 cat /etc/mtproxy/secret\033[0m"
-echo "删除mtproxy及其守护进程请运行： wget --no-check-certificate https://raw.githubusercontent.com/chummumm/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh"
+echo "删除mtproxy及其守护进程请运行： wget --no-check-certificate https://raw.githubusercontent.com/pipixia244/one-key-mtp/master/deletemtproxy.sh && bash deletemtproxy.sh"
